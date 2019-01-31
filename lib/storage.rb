aed3793241d88bed09dec1d96f4044db50a67652
Aleksey Tsibulnikov <aleksey6689@gmail.com>
2019-01-31 16:35:24 +0300

    redis storage, application settings, refactoring

--- lib/storage.rb
+++ lib/storage.rb
@@ -1,54 +1,63 @@
 class Storage
 
-  @@records = []
-
-  class << self
-
-    def trackers_by(attribute, value)
-      @@records.select { |record| record[attribute] == value }
+  def initialize(expires_at = 6, adapter = :ruby)
+    @adapter = adapter
+    @expires_at = expires_at
+    @records = []
+    if adapter_redis?
+      @redis_store = Redis.new
     end
-
-    def actual_records(records)
-      current_time = Time.now
-      records.select { |record| record[:created_at] > current_time - 50 }
-    end
-
-    def actual_trackers_by(attribute, value)
-      trackers = self.trackers_by(attribute, value)
-      self.actual_records(trackers)
-    end
-
-    def get_all_by_video(video_id)
-      @@records.select { |record| record[:video_id ]== video_id }
-    end
-
-    def delete_expired_records
-      current_time = Time.now
-      @@records.delete_if { |record| record[:created_at] < current_time - 50 }
-    end
-
-    def all
-      @@records
-    end
-
   end
 
-  def initialize(attributes)
-    @customer_id, @video_id = attributes[:customer_id], attributes[:video_id]
+  def records
+    if adapter_redis?
+      @records = []
+      @redis_store.scan_each(match: 'storage:*') do |key|
+        @records << JSON.parse(@redis_store.get(key)).to_h
+      end
+    end
+    @records
   end
 
-  def save
-    if valid?
-      attributes = { customer_id: @customer_id, video_id: @video_id, created_at: Time.now }
-      @@records << attributes
-      true
+  def add(attributes)
+    attributes['created_at'] = Time.now.to_i
+    if adapter_redis?
+      @redis_store.set(
+        "storage:customer#{attributes['customer_id']}:#{attributes['created_at']}",
+        attributes.to_json,
+        ex: @expires_at
+      )
     else
-      false
+      @records << attributes
     end
   end
 
-  def valid?
-    @customer_id && @customer_id
+  def trackers_by(attribute, value)
+    records.select do |record|
+      record[attribute.to_s] == value
+    end
+  end
+
+  def actual_records(trackers)
+    unix_current_time = Time.now.to_i
+    puts 'actual_records', trackers
+    trackers.select do |record|
+      record['created_at'] > unix_current_time - @expires_at
+    end
+  end
+
+  def actual_trackers_by(attribute, value)
+    trackers = trackers_by(attribute, value)
+    actual_records(trackers)
+  end
+
+  def delete_expired_records
+    unix_current_time = Time.now.to_i
+    records.delete_if { |record| record['created_at'] < unix_current_time - @expires_at }
+  end
+
+  def adapter_redis?
+    @adapter == :redis
   end
 
 end
